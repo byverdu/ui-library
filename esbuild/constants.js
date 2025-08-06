c7f074003ab52b88380dc664bdd1840db99f0ced
Albert Vallverdu <byverdu@gmail.com>
2025-08-06 18:14:27 +0100

    Upgrade pnpm lockfile version to 9.0, streamline dependency structure, and add MUI exports for easier access to Material-UI components.

--- esbuild/constants.js
+++ esbuild/constants.js
@@ -1,7 +1,7 @@
 const OUT_BASE = 'src';
 const OUT_DIR = 'dist';
 const LIB_DIR = 'lib';
-const EXTRA_LIB_MODULES = ['hooks', 'theme'];
+const EXTRA_LIB_MODULES = ['hooks', 'theme', 'mui'];
 const EXTERNALS_MODULES = ['./*', './*/index'];
 
 export { OUT_BASE, OUT_DIR, LIB_DIR, EXTERNALS_MODULES, EXTRA_LIB_MODULES };
